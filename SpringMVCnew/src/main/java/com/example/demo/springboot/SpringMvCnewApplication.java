54442a8f9874a5d1540e318daa46ea9f66bc1527
Shivam Singhal <shivam.singhal@tavant.com>
2022-08-04 12:36:09 +0530

    Spring MVC- hello and index

---
+++ SpringMVCnew/src/main/java/com/example/demo/springboot/SpringMvCnewApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvCnewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvCnewApplication.class, args);
+	}
+
+}
